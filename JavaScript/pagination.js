7cf95d4fa6894eafec8fce822928c16d6c6e6a0b
Wendy <cyw880219@gmail.com>
2022-02-16 16:19:59 +0800

    加上 .prevent 修飾符停用元素本身的預設行為

--- JavaScript/pagination.js
+++ JavaScript/pagination.js
@@ -3,7 +3,7 @@ export default {
     template: `<nav aria-label="Page navigation example">
     <ul class="pagination">
       <li class="page-item" :class="{ disabled: !pages.has_pre }">
-        <a class="page-link" href="#" aria-label="Previous">
+        <a class="page-link" href="#" aria-label="Previous" @click.prevent="$emit('get-product', pages.current_page - 1)">
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
@@ -11,10 +11,10 @@ export default {
       :class="{ active: page === pages.current_page }"
       v-for="page in pages.total_pages" :key="page +'page'">
         <a class="page-link" href="#" 
-        @click="$emit('get-product', page)">{{page}}</a>
+        @click.prevent="$emit('get-product', page)">{{page}}</a>
       </li>
       <li class="page-item"  :class="{ disabled: !pages.has_next }">
-        <a class="page-link" href="#" aria-label="Next">
+        <a class="page-link" href="#" aria-label="Next" @click.prevent="$emit('get-product', pages.current_page + 1)">
           <span aria-hidden="true">&raquo;</span>
         </a>
       </li>
@@ -23,3 +23,4 @@ export default {
 }
 //加上Active視覺效果
 //事件發送使用@click="$emit"
+//@click.prevent="事件名稱"，.prevent 修飾符是用於停止預設行為的發生
